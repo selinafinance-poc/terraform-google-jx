a6a9af6a4bb7f26c23ed3d498577cde8e7f8cb29
tgelpi <ted.gelpi@gmail.com>
2023-05-07 17:38:02 -0400

    feat: enable cluster version setting

--- modules/cluster/main.tf
+++ modules/cluster/main.tf
@@ -27,6 +27,7 @@ data "google_container_engine_versions" "jx_cluster_version" {
   location       = var.cluster_location
   version_prefix = var.cluster_version
 }
+
 resource "google_container_cluster" "jx_cluster" {
   name                      = var.cluster_name
   description               = "jenkins-x cluster"
@@ -36,9 +37,9 @@ resource "google_container_cluster" "jx_cluster" {
   enable_kubernetes_alpha   = var.enable_kubernetes_alpha
   enable_legacy_abac        = var.enable_legacy_abac
   enable_shielded_nodes     = var.enable_shielded_nodes
-  remove_default_node_pool  = (var.cluster_version != "" ) ? false : true
-  node_version              = (var.cluster_version != "" ) ? data.google_container_engine_versions.jx_cluster_version.latest_node_version : ""
-  min_master_version        = (var.cluster_version != "" ) ? data.google_container_engine_versions.jx_cluster_version.latest_node_version : ""
+//   remove_default_node_pool  = (var.cluster_version != "" ) ? false : true
+//   node_version              = (var.cluster_version != "" ) ? data.google_container_engine_versions.jx_cluster_version.latest_node_version : ""
+//   min_master_version        = (var.cluster_version != "" ) ? data.google_container_engine_versions.jx_cluster_version.latest_node_version : ""
   initial_node_count        = var.initial_cluster_node_count
   logging_service           = var.logging_service
   monitoring_service        = var.monitoring_service
